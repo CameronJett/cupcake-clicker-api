983c66e004fe6a6ef57fd1d13303992610046a63
Cameron Jett <cameron.jett18@gmail.com>
2020-05-10 16:37:49 -0400

    Create POST api endpoint

--- src/main/java/com/jett/tutorial/cupcakeclickerapi/Controller/UserController.java
+++ src/main/java/com/jett/tutorial/cupcakeclickerapi/Controller/UserController.java
@@ -1,20 +1,25 @@
 package com.jett.tutorial.cupcakeclickerapi.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import com.jett.tutorial.cupcakeclickerapi.Model.User;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin
 public class UserController {
 
-	@RequestMapping("/{userName}")
+	@GetMapping("/{userName}")
 	public User getUserByName(@PathVariable("userName") String name) {
 		return new User(name, 0);
 	}
 
+	@PostMapping("")
+	public User createNewUser(@RequestBody String name) {
+		return new User(name, 0);
+	}
 }
--- src/test/java/com/jett/tutorial/cupcakeclickerapi/Controller/UserControllerTest.java
+++ src/test/java/com/jett/tutorial/cupcakeclickerapi/Controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.jett.tutorial.cupcakeclickerapi.Controller;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,7 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,7 +25,16 @@ public class UserControllerTest {
 
 	@Test
 	public void shouldReturnNameOnGetRequest() throws Exception {
-		this.mockMvc.perform(get("/Boss")).andDo(print()).andExpect(status().isOk())
+		mockMvc.perform(get("/Boss")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Boss")));
+    }
+    
+	@Test
+	public void shouldReturnUserOnPostRequest() throws Exception {
+        mockMvc.perform(post("/").contentType(MediaType.APPLICATION_JSON)
+            .content("{\"Boss\"}"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Boss")));
 	}
 }
