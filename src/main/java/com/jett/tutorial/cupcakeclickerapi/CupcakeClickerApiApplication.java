9c313d6eed27149347a53b77b92eaacb255efe91
Cameron Jett <cameron.jett18@gmail.com>
2020-05-10 01:01:07 -0400

    initial commit

---
+++ src/main/java/com/jett/tutorial/cupcakeclickerapi/CupcakeClickerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jett.tutorial.cupcakeclickerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupcakeClickerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupcakeClickerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jett/tutorial/cupcakeclickerapi/CupcakeClickerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jett.tutorial.cupcakeclickerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupcakeClickerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
